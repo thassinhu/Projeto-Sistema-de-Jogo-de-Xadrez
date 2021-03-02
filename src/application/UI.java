10ef5e528feb8158688d543570c8c493fd5d4ecf
Thassius Carrion <thassius.carrion@hotmail.com>
2021-03-02 15:14:42 -0300

    Improvement - in UI class with changes on interface

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,6 +54,7 @@ public class UI {
 	}
 	
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
@@ -72,28 +73,27 @@ public class UI {
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
-	
+		System.out.println(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "--- JOGO DE XADREZ DO MENDES ---" + ANSI_RESET);
 		for(int i=0; i<pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_CYAN + (8 - i) + " " + ANSI_RESET);
 			for (int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
-		
+		System.out.print(ANSI_CYAN + "  a b c d e f g h" + ANSI_RESET);
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
-		
+		System.out.println(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "--- JOGO DE XADREZ DO MENDES ---" + ANSI_RESET);
 		for(int i=0; i<pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_CYAN + (8 - i) + " " + ANSI_RESET);
 			for (int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.print(ANSI_CYAN + "  a b c d e f g h" + ANSI_RESET);
 		
 	}
 	
